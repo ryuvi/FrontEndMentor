e675f53eb37becb604966dbbdadc4acf7692b9e1
ryuvi <vicenters10@gmail.com>
2020-12-02 21:38:42 -0300

    Made some challenges

--- script.js
+++ script.js
@@ -14,25 +14,32 @@ const newbie = [
   {
     url: "ping-coming-soon-page",
   },
+  {
+    url: "intro-component-with-signup-form",
+  },
 ];
 
 const list = document.getElementById("newbie");
 
-newbie.forEach(({ url }) => {
-  const listItem = document.createElement("li");
-  listItem.setAttribute("class", "blocks");
+projects(newbie);
 
-  listItem.innerHTML = `
-        <img src="./src/${url}/design/desktop-preview.jpg" alt="${url}">
-        <div>
-            <a href="./src/${url}/index.html">
-                <i class="far fa-eye"></i>
-            </a>
-            <a href="https://github.com/ryuvi/FrontEndMentor/tree/Main/src/${url}">
-                <i class="fas fa-code"></i>
-            </a>
-        </div>
-    `;
+function projects(project) {
+  project.forEach(({ url }) => {
+    const listItem = document.createElement("li");
+    listItem.setAttribute("class", "blocks");
 
-  list.appendChild(listItem);
-});
+    listItem.innerHTML = `
+          <img src="./src/${url}/design/desktop-preview.jpg" alt="${url}">
+          <div>
+              <a href="./src/${url}/index.html">
+                  <i class="far fa-eye"></i>
+              </a>
+              <a href="https://github.com/ryuvi/FrontEndMentor/tree/Main/src/${url}">
+                  <i class="fas fa-code"></i>
+              </a>
+          </div>
+      `;
+
+    list.appendChild(listItem);
+  });
+}
