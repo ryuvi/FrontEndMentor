29c86f12dbcb1f7084aaf9f478e99a91871a2b82
ryuvi <vicenters10@gmail.com>
2020-12-08 22:18:55 -0300

    added jokenpo

--- script.js
+++ script.js
@@ -18,10 +18,23 @@ const newbie = [
     url: "intro-component-with-signup-form",
   },
 ];
+const junior = [];
+const intermediate = [];
+const advance = [
+  {
+    url: "rock-paper-scissors"
+  }
+];
 
-const list = document.getElementById("newbie");
+const liNewbie = document.getElementById("newbie");
+const liJunior = document.getElementById("junior");
+const liIntermediate = document.getElementById("intermediate");
+const liAdvance = document.getElementById("advance");
 
 projects(newbie);
+projects(junior);
+projects(intermediate);
+projects(advance);
 
 function projects(project) {
   project.forEach(({ url }) => {
@@ -40,6 +53,9 @@ function projects(project) {
           </div>
       `;
 
-    list.appendChild(listItem);
+    if (project == newbie) liNewbie.appendChild(listItem);
+    if (project == junior) liJunior.appendChild(listItem);
+    if (project == intermediate) liIntermediate.appendChild(listItem);
+    if (project == advance) liAdvance.appendChild(listItem);
   });
 }
